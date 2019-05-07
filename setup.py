64932dac0a234cf68680e8955b91575c089998fd
flightmaestro <48744447+flightmaestro@users.noreply.github.com>
2019-05-07 08:07:47 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cnnplantdiseasedetection',
+    version='1.0',
+    description='Plant disease detection module',
+    author='Courage ARMAH',
+    author_email='darielarmah@yahoo.com',
+    packages=['cnnplantdiseasedetection'],  #same as name
+)
