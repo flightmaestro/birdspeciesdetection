d2ed3f3c99c7b376bdea5bcff1a22e64921bf562
Courage Armah <48744447+flightmaestro@users.noreply.github.com>
2024-01-31 17:13:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='cnnplantdiseasedetection',
+    name='birdspeciesdetection',
     version='1.0',
-    description='Plant disease detection module',
+    description='Bird species detection module',
     author='Courage ARMAH',
     author_email='darielarmah@yahoo.com',
-    packages=['cnnplantdiseasedetection'],  #same as name
+    packages=['cnnbirdspeciesdetection'],  #same as name
 )
